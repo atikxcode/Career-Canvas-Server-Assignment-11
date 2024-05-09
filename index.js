6bfe2b920cf1f0cd8ef5568a1fc22c3c16f13478
atikxcode <liadbd20@gmail.com>
2024-05-09 23:02:24 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+
+app.use(cors());
+app.use(express.json());
+
+
+
+app.get('/', (req, res) => {
+  res.send('Job Seeking Server');
+})
+
+app.listen(port, () => {
+  console.log(`Job server is running on port: ${port}`);
+})
