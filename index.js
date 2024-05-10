5b40539dd6eed876b970d26549492de558e77ff5
atikxcode <liadbd20@gmail.com>
2024-05-10 06:26:41 +0600

    added post method

--- index.js
+++ index.js
@@ -29,6 +29,19 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const jobCollection = client.db('jobsite').collection('joblisted');
+    
+
+    app.post('/joblisted', async(req, res) => {
+      const newJobPost = req.body;
+      console.log(newJobPost);
+      // res.header("Access-Control-Allow-Origin", "*");
+      const result = await jobCollection.insertOne(newJobPost);
+      res.send(result);
+    })
+
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
