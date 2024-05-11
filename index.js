d0e5ee9579e2e28b918989cebb136a393326c888
atikxcode <liadbd20@gmail.com>
2024-05-12 04:52:40 +0600

    apply for job and storing in another databse

--- index.js
+++ index.js
@@ -87,6 +87,56 @@ async function run() {
      })
 
 
+     const appliedJobCollection = client.db('jobsite').collection('appliedjob');
+
+     app.put('/joblisted/:id/apply', async (req, res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+
+      try{
+        const jobListing = await jobCollection.findOne(filter);
+
+        if(!jobListing){
+          return res.status(404).send({success: false, message: 'Job listing not found'});
+
+        }
+
+        const deadlineDate = new Date(jobListing.deadline);
+        const currentDate = new Date();
+        if(currentDate > deadlineDate){
+          return res.status(400).send({success: false, message: 'Deadline for applying has passed'});
+
+        }
+
+        const updatedTotalApplied = jobListing.totalApplied + 1;
+
+
+        const result = await jobCollection.updateOne(filter, {$set: {totalApplied: updatedTotalApplied}});
+
+        res.send({success: true, message: 'Applied Successfully', updatedTotalApplied});
+        
+        app.post('/appliedjob', async(req, res) => {
+          const appliedJobs = req.body;
+          console.log(appliedJobs);
+          // res.header("Access-Control-Allow-Origin", "*");
+          const result = await appliedJobCollection.insertOne(appliedJobs);
+          res.send(result);
+        })
+
+      } catch (error) {
+        console.error('Error applying to job: ', error);
+        res.status(500).send({success: false, message: 'Internal server error'});
+      }
+     })
+
+
+     app.get('/appliedjob', async(req, res) => {
+      const cursor = appliedJobCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+
 
      app.delete('/joblisted/:id', async(req, res) => {
       const id = req.params.id;
