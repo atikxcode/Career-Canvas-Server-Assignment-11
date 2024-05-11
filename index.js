224dc082b0617a9eb66f1cec4ccb5fbd853353fe
atikxcode <liadbd20@gmail.com>
2024-05-11 08:04:45 +0600

    added the delete option

--- index.js
+++ index.js
@@ -60,6 +60,21 @@ async function run() {
       res.send(result);
     })
 
+
+ 
+
+
+     app.delete('/joblisted/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await jobCollection.deleteOne(query);
+      // res.header("Access-Control-Allow-Origin", "*");
+      res.send(result);
+    })
+
+
+
+
     
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
